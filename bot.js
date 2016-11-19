1a1a9c47bc5571d96b66c23d6906c9c282979c93
Louis Trezzini <louis.trezzini@eleves.enpc.fr>
2016-11-19 14:21:33 +0100

    Remove useless headers

--- bot.js
+++ bot.js
@@ -15,27 +15,28 @@ request.get({
         url: apiUrl + '/game/play',
         headers: {
             "accept": "*/*",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "fr,en-US;q=0.8,en;q=0.6",
-            "content-type": "application/x-www-form-urlencoded",
             "Cookie": "_massive_boggle_session=BAh7CSIQX2NzcmZfdG9rZW4iMVBLRHQxN1I5SS8xNmpGWUVpUllpVyt6blVqRnNOM2JBZHBrektjc1Z5WUk9IhBwbGF5ZXJfbmFtZSISVmlzaXRldXJfNDExMiIPc2Vzc2lvbl9pZCIlYTFiYzMyNmQ2Y2YzNWUwZmQ0MGFkY2ZlMzYzNjBmY2QiDnBsYXllcl9pZGkDo8AH--936e7093929508e79f7b4fa9d227e11a5f97d26a",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
             "x-requested-with": "XMLHttpRequest",
         }
     }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        // console.log(body)
+        if (!error && response.statusCode == 200) {
+            // console.log(body)
 
-        let $ = cheerio.load(body)
+            let $ = cheerio.load(body)
 
-        token = $('meta[name=csrf_token]').attr('content')
-        playerName = $('#player_name').text()
+            token = $('meta[name=csrf_token]').attr('content')
+            playerName = $('#player_name').text()
 
-        console.log(playerName)
+            console.log(playerName)
 
-        getBoard()
+            getBoard()
+        }
+        else {
+            console.log(error, response.statusCode, body)
+        }
     }
-})
+)
 
 function getBoard(){
     request.post({
@@ -63,12 +64,7 @@ function sendWord(word) {
         url: apiUrl + '/game/guess',
         headers: {
             "accept": "*/*",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "fr,en-US;q=0.8,en;q=0.6",
-            "content-type": "application/x-www-form-urlencoded",
             "Cookie": "_massive_boggle_session=BAh7CSIQX2NzcmZfdG9rZW4iMVBLRHQxN1I5SS8xNmpGWUVpUllpVyt6blVqRnNOM2JBZHBrektjc1Z5WUk9IhBwbGF5ZXJfbmFtZSISVmlzaXRldXJfNDExMiIPc2Vzc2lvbl9pZCIlYTFiYzMyNmQ2Y2YzNWUwZmQ0MGFkY2ZlMzYzNjBmY2QiDnBsYXllcl9pZGkDo8AH--936e7093929508e79f7b4fa9d227e11a5f97d26a",
-            // "Origin": "http://massiveboggle.fr",
-            // "Referer": "http://massiveboggle.fr/game/play",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
             "x-requested-with": "XMLHttpRequest",
         },
